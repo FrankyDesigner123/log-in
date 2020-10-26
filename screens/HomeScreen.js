ddd0103147513f5168d9123542300b00de8d46f4
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-26 08:59:30 +0100

    Fetching Tokens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const HomeScreen = () => {
+const HomeScreen = props => {
+
+    const loadProfile = async () => {
+        const token = await AsyncStorage.getItem('token');
+        if(!token){
+            props.navigation.navigate('Login')
+        }
+        console.log(token);
+
+    }
+
+    useEffect(() => {
+        loadProfile();
+    });
+
     return(
         <View>
             <Text>Home Screen</Text>
